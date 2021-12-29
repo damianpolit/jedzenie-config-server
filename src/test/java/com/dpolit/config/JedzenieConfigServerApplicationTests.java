35dc06649cdc7fb59ce6e2d788f200bd354110ea
Damian <damianpolit@interia.pl>
2021-12-29 19:21:55 +0100

    Initial commit

---
+++ src/test/java/com/dpolit/config/JedzenieConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpolit.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedzenieConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
